0fef60c130395a5c3cd09e68c75487cea9f98014
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2024-09-26 10:58:16 +0300

    [UPDATE] update profile

--- src/modules/profile/studentProfile.jsx
+++ src/modules/profile/studentProfile.jsx
@@ -11,6 +11,7 @@ import {
 import PersonalDetails from "./components/PersonalDetails/personalDetails";
 import ProfileCard from "./components/ProfileCard/profileCard";
 import ContentWrapper from "../../components/styled/ContentWrapper";
+import PageWrapper from "../../components/styled/PageWrapper";
 
 const StudentProfile = () => {
   // Dummy data for certificates
@@ -45,37 +46,37 @@ const StudentProfile = () => {
       <CustomAppBar {...secondAppBarConfig} />
       <Courses title="الملف الشّخصي" />
 
-      <Box>
-        <Container
-          maxWidth={false} // This removes the maxWidth limitation and makes it full width
-          style={{ marginTop: "3rem", marginBottom: "3rem", padding: "0 70px" }} // Adjust padding equally on both sides
-        >
+      <PageWrapper>
+        <ContentWrapper>
           <ProfileCard />
           <PersonalDetails />
 
           <ViewAll title="الدّورات المسجّل عليها:" showAllText="عرض الكل" />
           {/* Wrap Grid in a Box to ensure even spacing */}
-          <ContentWrapper>
-            {/* Responsive Grid Layout */}
-            <Grid container spacing={5}>
-              {Array.from({ length: 3 }).map((_, index) => (
-                <Grid
-                  item
-                  xs={12} // Full width on extra small screens (mobile)
-                  sm={6} // 2 items per row on small screens (tablets)
-                  md={4} // 3 items per row on medium screens (desktop)
-                  lg={4} // 4 items per row on large screens
-                  key={index}
-                >
-                  <Course />
-                </Grid>
-              ))}
-            </Grid>
-          </ContentWrapper>
 
+          {/* Responsive Grid Layout */}
+          <Grid container spacing={5}>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <Grid
+                item
+                xs={12} // Full width on extra small screens (mobile)
+                sm={6} // 2 items per row on small screens (tablets)
+                md={4} // 3 items per row on medium screens (desktop)
+                lg={4} // 4 items per row on large screens
+                key={index}
+              >
+                <Course />
+              </Grid>
+            ))}
+          </Grid>
+        </ContentWrapper>
+      </PageWrapper>
+
+      <PageWrapper>
+        <ContentWrapper>
           <ViewAll title="الشّهادات الحاصل عليها:" showAllText="عرض الكل" />
           {/* Display Certificates */}
-          <Box sx={{ width: "100%", padding: "0 16px" }}>
+         
             <Grid container spacing={5}>
               {certificateData.map((certificate) => (
                 <Grid item xs={12} sm={6} md={4} lg={4} key={certificate.id}>
@@ -89,9 +90,8 @@ const StudentProfile = () => {
                 </Grid>
               ))}
             </Grid>
-          </Box>
-        </Container>
-      </Box>
+        </ContentWrapper>
+      </PageWrapper>
     </>
   );
 };
